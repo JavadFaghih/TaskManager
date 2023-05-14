12cfd9f89b8e9714f857e03fcbff3f7595675c2e
Javad Faghih <Javad.4145@gmail.com>
2023-05-14 23:25:22 +0300

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Javad on 2/24/1402 AP.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
